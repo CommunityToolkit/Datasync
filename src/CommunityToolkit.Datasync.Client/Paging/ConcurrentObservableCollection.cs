47d38e35e7293e8d0e861a2862284470ab0d5106
Adrian Hall <photoadrian@outlook.com>
2024-08-04 12:27:21 -0700

    feat: ConcurrentObservableCollection and extension methods for IDatasyncQueryable (#56)

---
+++ src/CommunityToolkit.Datasync.Client/Paging/ConcurrentObservableCollection.cs
@@ -0,0 +1,195 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CommunityToolkit.Datasync.Client;
+
+/// <summary>
+/// A thread-safe implementation of the <see cref="ObservableCollection{T}"/> that allows us
+/// to add/replace/remove ranges without notifying more than once.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class ConcurrentObservableCollection<T> : ObservableCollection<T>
+{
+    private readonly SynchronizationContext context = SynchronizationContext.Current!;
+    private bool suppressNotification = false;
+
+    /// <summary>
+    /// Creates a new (empty) collection.
+    /// </summary>
+    public ConcurrentObservableCollection() : base()
+    {
+    }
+
+    /// <summary>
+    /// Creates a new collection seeded with the provided information.
+    /// </summary>
+    /// <param name="list">The information to be used for seeding the collection.</param>
+    public ConcurrentObservableCollection(IEnumerable<T> list) : base(list)
+    {
+    }
+
+    /// <summary>
+    /// Replaces the contents of the observable collection with new contents.
+    /// </summary>
+    /// <param name="collection">The new collection.</param>
+    public void ReplaceAll(IEnumerable<T> collection)
+    {
+        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+        try
+        {
+            this.suppressNotification = true;
+            Clear();
+            foreach (T? item in collection)
+            {
+                Add(item);
+            }
+        }
+        finally
+        {
+            this.suppressNotification = false;
+        }
+
+        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+    }
+
+    /// <summary>
+    /// Adds a collection to the existing collection.
+    /// </summary>
+    /// <param name="collection">The collection of records to add.</param>
+    /// <returns><c>true</c> if any records were added; <c>false</c> otherwise.</returns>
+    public bool AddRange(IEnumerable<T> collection)
+    {
+        ArgumentNullException.ThrowIfNull(collection, nameof(collection));
+        bool changed = false;
+        try
+        {
+            this.suppressNotification = true;
+            foreach (T? item in collection)
+            {
+                Add(item);
+                changed = true;
+            }
+        }
+        finally
+        {
+            this.suppressNotification = false;
+        }
+
+        if (changed)
+        {
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        return changed;
+    }
+
+    /// <summary>
+    /// Adds an item within a collection only if there are no items identified by the match function.
+    /// </summary>
+    /// <param name="match">The match function.</param>
+    /// <param name="item">The item to add.</param>
+    /// <returns><c>true</c> if the item was added, <c>false</c> otherwise.</returns>
+    public bool AddIfMissing(Func<T, bool> match, T item)
+    {
+        ArgumentNullException.ThrowIfNull(match, nameof(match));
+        ArgumentNullException.ThrowIfNull(item, nameof(item));
+        if (!this.Any(match))
+        {
+            Add(item);
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Removes items within the collection based on a match function.
+    /// </summary>
+    /// <param name="match">The match predicate.</param>
+    /// <returns><c>true</c> if an item was removed, <c>false</c> otherwise.</returns>
+    public bool RemoveIf(Func<T, bool> match)
+    {
+        ArgumentNullException.ThrowIfNull(match, nameof(match));
+        T[] itemsToRemove = this.Where(match).ToArray();
+        foreach (T? item in itemsToRemove)
+        {
+            int idx = IndexOf(item);
+            RemoveAt(idx);
+        }
+
+        return itemsToRemove.Length > 0;
+    }
+
+    /// <summary>
+    /// Replaced items within the collection with a (single) replacement based on a match function.
+    /// </summary>
+    /// <param name="match">The match predicate.</param>
+    /// <param name="replacement">The replacement item.</param>
+    /// <returns><c>true</c> if an item was replaced, <c>false</c> otherwise.</returns>
+    public bool ReplaceIf(Func<T, bool> match, T replacement)
+    {
+        ArgumentNullException.ThrowIfNull(match, nameof(match));
+        ArgumentNullException.ThrowIfNull(replacement, nameof(replacement));
+        T[] itemsToReplace = this.Where(match).ToArray();
+        foreach (T? item in itemsToReplace)
+        {
+            int idx = IndexOf(item);
+            this[idx] = replacement;
+        }
+
+        return itemsToReplace.Length > 0;
+    }
+
+    /// <summary>
+    /// Event trigger to indicate that the collection has changed in a thread-safe way.
+    /// </summary>
+    /// <param name="e">The event arguments</param>
+    protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+    {
+        if (SynchronizationContext.Current == this.context)
+        {
+            RaiseCollectionChanged(e);
+        }
+        else
+        {
+            this.context.Send(RaiseCollectionChanged, e);
+        }
+    }
+
+    /// <summary>
+    /// Event trigger to indicate that a property has changed in a thread-safe way.
+    /// </summary>
+    /// <param name="e">The event arguments</param>
+    protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        if (SynchronizationContext.Current == this.context)
+        {
+            RaisePropertyChanged(e);
+        }
+        else
+        {
+            this.context.Send(RaisePropertyChanged, e);
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    private void RaiseCollectionChanged(object? param)
+    {
+        if (!this.suppressNotification)
+        {
+            base.OnCollectionChanged((NotifyCollectionChangedEventArgs)param!);
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    private void RaisePropertyChanged(object? param)
+    {
+        base.OnPropertyChanged((PropertyChangedEventArgs)param!);
+    }
+}
---
+++ src/CommunityToolkit.Datasync.Client/Query/IAsyncEnumerableExtensions.cs
@@ -0,0 +1,218 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+
+namespace CommunityToolkit.Datasync.Client;
+
+/// <summary>
+/// A set of extension methods for the <see cref="IAsyncEnumerable{T}"/> interface.
+/// </summary>
+/// <remarks>
+/// These are mostly duplicative of the ones in System.Linq.Async, but are repeated
+/// here so we don't have to take a dependency on a third party library that we don't
+/// control.
+/// </remarks>
+internal static class IAsyncEnumerableExtensions
+{
+    /// <summary>
+    /// Creates an array from an async-enumerable sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source async-enumerable sequence to get an array of elements for.</param>
+    /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+    /// <returns>An async-enumerable sequence containing a single element with an array containing all the elements of the source sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+    /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+    internal static async ValueTask<TSource[]> ToDatasyncArrayAsync<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        List<TSource> list = await source.ToDatasyncListAsync(cancellationToken).ConfigureAwait(false);
+        return [.. list];
+    }
+
+    /// <summary>
+    /// Creates a dictionary from an async-enumerable sequence according to a specified key selector function.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <typeparam name="TKey">The type of the dictionary key computed for each element in the source sequence.</typeparam>
+    /// <param name="source">An async-enumerable sequence to create a dictionary for.</param>
+    /// <param name="keySelector">A function to extract a key from each element.</param>
+    /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+    /// <returns>An async-enumerable sequence containing a single element with a dictionary mapping unique key values onto the corresponding source sequence's element.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="keySelector"/> is null.</exception>
+    /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+    internal static ValueTask<Dictionary<TKey, TSource>> ToDatasyncDictionaryAsync<TSource, TKey>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, CancellationToken cancellationToken = default) where TKey : notnull
+        => ToDatasyncDictionaryAsync(source, keySelector, comparer: null, cancellationToken);
+
+    /// <summary>
+    /// Creates a dictionary from an async-enumerable sequence according to a specified key selector function, and a comparer.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <typeparam name="TKey">The type of the dictionary key computed for each element in the source sequence.</typeparam>
+    /// <param name="source">An async-enumerable sequence to create a dictionary for.</param>
+    /// <param name="keySelector">A function to extract a key from each element.</param>
+    /// <param name="comparer">An equality comparer to compare keys.</param>
+    /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+    /// <returns>An async-enumerable sequence containing a single element with a dictionary mapping unique key values onto the corresponding source sequence's element.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="keySelector"/> or <paramref name="comparer"/> is null.</exception>
+    /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+    internal static async ValueTask<Dictionary<TKey, TSource>> ToDatasyncDictionaryAsync<TSource, TKey>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer, CancellationToken cancellationToken = default) where TKey : notnull
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        ArgumentNullException.ThrowIfNull(keySelector, nameof(keySelector));
+        Dictionary<TKey, TSource> d = new(comparer);
+        await foreach (TSource? item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+        {
+            TKey key = keySelector(item);
+            d.Add(key, item);
+        }
+
+        return d;
+    }
+
+    /// <summary>
+    /// Converts an async-enumerable sequence to an enumerable sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">An async-enumerable sequence to convert to an enumerable sequence.</param>
+    /// <returns>The enumerable sequence containing the elements in the async-enumerable sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+    internal static IEnumerable<TSource> ToDatasyncEnumerable<TSource>(this IAsyncEnumerable<TSource> source)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        return Core(source);
+
+        static IEnumerable<TSource> Core(IAsyncEnumerable<TSource> source)
+        {
+            IAsyncEnumerator<TSource> e = source.GetAsyncEnumerator(default);
+            try
+            {
+                while (true)
+                {
+                    if (!DatasyncWait(e.MoveNextAsync()))
+                    {
+                        break;
+                    }
+
+                    yield return e.Current;
+                }
+            }
+            finally
+            {
+                DatasyncWait(e.DisposeAsync());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Creates a hash set from an async-enumerable sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source async-enumerable sequence to get a hash set of elements for.</param>
+    /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+    /// <returns>An async-enumerable sequence containing a single element with a hash set containing all the elements of the source sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+    /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+    internal static ValueTask<HashSet<TSource>> ToDatasyncHashSetAsync<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken cancellationToken = default)
+        => ToDatasyncHashSetAsync(source, comparer: null, cancellationToken);
+
+    /// <summary>
+    /// Creates a hash set from an async-enumerable sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source async-enumerable sequence to get a hash set of elements for.</param>
+    /// <param name="comparer">An equality comparer to compare elements of the sequence.</param>
+    /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+    /// <returns>An async-enumerable sequence containing a single element with a hash set containing all the elements of the source sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+    /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+    internal static async ValueTask<HashSet<TSource>> ToDatasyncHashSetAsync<TSource>(this IAsyncEnumerable<TSource> source, IEqualityComparer<TSource>? comparer, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        HashSet<TSource> set = new(comparer);
+        await foreach (TSource? item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+        {
+            _ = set.Add(item);
+        }
+
+        return set;
+    }
+
+    /// <summary>
+    /// Creates a list from an async-enumerable sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source async-enumerable sequence to get a list of elements for.</param>
+    /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+    /// <returns>A list containing all the elements of the source sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+    internal static async ValueTask<List<TSource>> ToDatasyncListAsync<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        List<TSource> list = [];
+        await foreach (TSource? item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+        {
+            list.Add(item);
+        }
+
+        return list;
+    }
+
+    /// <summary>
+    /// Creates a <see cref="ConcurrentObservableCollection{T}"/> from an async-enumerable sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source async-enumerable sequence to get a list of elements for.</param>
+    /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+    /// <returns>An observable collection containing all the elements of the source sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+    internal static ValueTask<ConcurrentObservableCollection<TSource>> ToDatasyncObservableCollectionAsync<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken cancellationToken = default)
+        => source.ToDatasyncObservableCollectionAsync([], cancellationToken);
+
+    /// <summary>
+    /// Updates a <see cref="ConcurrentObservableCollection{T}"/> from an async-enumerable sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source async-enumerable sequence to get a list of elements for.</param>
+    /// <param name="existingCollection">The existing observable collection object.</param>
+    /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+    /// <returns>The updated observable collection containing all the elements of the source sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+    internal static async ValueTask<ConcurrentObservableCollection<TSource>> ToDatasyncObservableCollectionAsync<TSource>(this IAsyncEnumerable<TSource> source, ConcurrentObservableCollection<TSource> existingCollection, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        List<TSource> list = await source.ToDatasyncListAsync(cancellationToken).ConfigureAwait(false);
+        existingCollection.ReplaceAll(list);
+        return existingCollection;
+    }
+
+    // NB: ValueTask and ValueTask<T> do not have to support blocking on a call to GetResult when backed by
+    //     an IValueTaskSource or IValueTaskSource<T> implementation. Convert to a Task or Task<T> to do so
+    //     in case the task hasn't completed yet.
+
+    private static void DatasyncWait(ValueTask task)
+    {
+        ValueTaskAwaiter awaiter = task.GetAwaiter();
+        if (!awaiter.IsCompleted)
+        {
+            task.AsTask().GetAwaiter().GetResult();
+            return;
+        }
+
+        awaiter.GetResult();
+    }
+
+    private static T DatasyncWait<T>(ValueTask<T> task)
+    {
+        ValueTaskAwaiter<T> awaiter = task.GetAwaiter();
+        if (!awaiter.IsCompleted)
+        {
+            return task.AsTask().GetAwaiter().GetResult();
+        }
+
+        return awaiter.GetResult();
+    }
+}
+
