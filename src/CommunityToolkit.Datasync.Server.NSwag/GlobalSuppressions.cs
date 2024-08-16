afb0671216b1c61fbdc8e2e4aca4cdc05abd8664
Adrian Hall <adhal@microsoft.com>
2024-08-16 15:30:23 -0700

    removed global suppressions

--- src/CommunityToolkit.Datasync.Server.NSwag/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "<Pending>", Scope = "member", Target = "~M:CommunityToolkit.Datasync.Server.NSwag.OpenApiDatasyncExtensions.SetResponse(NSwag.OpenApiOperation,System.Net.HttpStatusCode,NJsonSchema.JsonSchema,System.Boolean)")]
