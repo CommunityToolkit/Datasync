ad522d60023663a0f36946569edb21c3cd644f84
Adrian Hall <photoadrian@outlook.com>
2024-09-27 10:31:53 -0700

    (#115) Specify culture for JSON serialization. (#117)

--- src/CommunityToolkit.Datasync.Server.Abstractions/Json/TimeOnlyConverter.cs
+++ src/CommunityToolkit.Datasync.Server.Abstractions/Json/TimeOnlyConverter.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -14,6 +15,7 @@ namespace CommunityToolkit.Datasync.Server.Abstractions.Json;
 public class TimeOnlyConverter : JsonConverter<TimeOnly>
 {
     private const string format = "HH:mm:ss.fff";
+    private static readonly CultureInfo culture = new("en-US");
 
     /// <inheritdoc />
     public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
@@ -21,5 +23,5 @@ public class TimeOnlyConverter : JsonConverter<TimeOnly>
 
     /// <inheritdoc />
     public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options)
-        => writer.WriteStringValue(value.ToString(format));
+        => writer.WriteStringValue(value.ToString(format, culture));
 }
--- tests/CommunityToolkit.Datasync.Server.Abstractions.Test/Json/SerializerTests.cs
+++ tests/CommunityToolkit.Datasync.Server.Abstractions.Test/Json/SerializerTests.cs
@@ -4,8 +4,11 @@
 
 using Azure.Core.Serialization;
 using CommunityToolkit.Datasync.Server.Abstractions.Json;
+using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+
+#pragma warning disable IDE0028 // Simplify collection initialization
 
 namespace CommunityToolkit.Datasync.Server.Abstractions.Test.Json;
 
@@ -37,4 +40,25 @@ public abstract class SerializerTests
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         ReadCommentHandling = JsonCommentHandling.Skip
     };
+
+    public static TheoryData<string> Locales => new()
+    {
+        "fr-FR",
+        "da-DA",
+        "en-US"
+    };
+
+    protected static void TestWithCulture(string culture, Action act)
+    {
+        CultureInfo currentCulture = Thread.CurrentThread.CurrentCulture;
+        Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+        try
+        {
+            act.Invoke();
+        }
+        finally
+        {
+            Thread.CurrentThread.CurrentCulture = currentCulture;
+        }
+    }
 }
