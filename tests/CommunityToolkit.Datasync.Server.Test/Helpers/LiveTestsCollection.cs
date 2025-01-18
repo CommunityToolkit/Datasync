63210cfc3efb7117452882fb85b3a89c3e82e231
Adrian Hall <adhal@microsoft.com>
2025-01-17 16:37:54 -0800

    (#205) AzureSQL query tests on the controller.

---
+++ tests/CommunityToolkit.Datasync.Server.Test/Helpers/LiveTestsCollection.cs
@@ -0,0 +1,22 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace CommunityToolkit.Datasync.Server.Test.Helpers;
+
+/// <summary>
+///  This can be used to share state between the various live tests.  It isn't used right now.
+/// </summary>
+public class DatabaseFixture
+{
+
+}
+
+[CollectionDefinition("LiveTestsCollection", DisableParallelization = true)]
+public class LiveTestsCollection : ICollectionFixture<DatabaseFixture>
+{
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
+
