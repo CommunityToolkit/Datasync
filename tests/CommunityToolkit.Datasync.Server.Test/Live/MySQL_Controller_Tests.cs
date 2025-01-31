85a0c72f03d14e684da73635773fddd51f6bca79
Adrian Hall <adhal@microsoft.com>
2025-01-31 12:53:53 -0800

    (#238) MySQL database tests use async lifetime.

--- tests/CommunityToolkit.Datasync.Server.Test/Live/MySQL_Controller_Tests.cs
+++ tests/CommunityToolkit.Datasync.Server.Test/Live/MySQL_Controller_Tests.cs
@@ -6,44 +6,53 @@ using CommunityToolkit.Datasync.Server.EntityFrameworkCore;
 using CommunityToolkit.Datasync.Server.Test.Helpers;
 using CommunityToolkit.Datasync.TestCommon.Databases;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 using Xunit.Abstractions;
 
 namespace CommunityToolkit.Datasync.Server.Test.Live;
 
 [ExcludeFromCodeCoverage]
 [Collection("LiveTestsCollection")]
-public class MySQL_Controller_Tests : LiveControllerTests<MysqlEntityMovie>
+public class MySQL_Controller_Tests(DatabaseFixture fixture, ITestOutputHelper output) : LiveControllerTests<MysqlEntityMovie>, IAsyncLifetime
 {
     #region Setup
-    private readonly DatabaseFixture _fixture;
     private readonly Random random = new();
-    private readonly string connectionString;
-    private readonly List<MysqlEntityMovie> movies;
+    private readonly string connectionString = Environment.GetEnvironmentVariable("DATASYNC_MYSQL_CONNECTIONSTRING");
+    private List<MysqlEntityMovie> movies = [];
 
-    public MySQL_Controller_Tests(DatabaseFixture fixture, ITestOutputHelper output) : base()
+    public async Task InitializeAsync()
     {
-        this._fixture = fixture;
-        this.connectionString = Environment.GetEnvironmentVariable("DATASYNC_MYSQL_CONNECTIONSTRING");
         if (!string.IsNullOrEmpty(this.connectionString))
         {
-            output.WriteLine($"MysqlIsInitialized = {this._fixture.MysqlIsInitialized}");
-            Context = MysqlDbContext.CreateContext(this.connectionString, output, clearEntities: !this._fixture.MysqlIsInitialized);
-            this.movies = Context.Movies.AsNoTracking().ToList();
-            this._fixture.MysqlIsInitialized = true;
+            // Note: we don't clear entities on every run to speed up the test runs.  This can only be done because
+            // the tests are read-only (associated with the query and get capabilities).  If the test being run writes
+            // to the database then change clearEntities to true.
+            output.WriteLine($"CosmosIsInitialized = {fixture.MysqlIsInitialized}");
+            Context = await MysqlDbContext.CreateContextAsync(this.connectionString, output, clearEntities: !fixture.MysqlIsInitialized);
+            this.movies = await Context.Movies.AsNoTracking().ToListAsync();
+            fixture.MysqlIsInitialized = true;
+        }
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (Context is not null)
+        {
+            await Context.DisposeAsync();
         }
     }
 
     private MysqlDbContext Context { get; set; }
 
-    protected override string DriverName { get; } = "PgSQL";
+    protected override string DriverName { get; } = "MySQL";
 
     protected override bool CanRunLiveTests() => !string.IsNullOrEmpty(this.connectionString);
 
-    protected override Task<MysqlEntityMovie> GetEntityAsync(string id)
-        => Task.FromResult(Context.Movies.AsNoTracking().SingleOrDefault(m => m.Id == id));
+    protected override async Task<MysqlEntityMovie> GetEntityAsync(string id)
+        => await Context.Movies.AsNoTracking().SingleOrDefaultAsync(m => m.Id == id);
 
-    protected override Task<int> GetEntityCountAsync()
-        => Task.FromResult(Context.Movies.Count());
+    protected override async Task<int> GetEntityCountAsync()
+        => await Context.Movies.CountAsync();
 
     protected override Task<IRepository<MysqlEntityMovie>> GetPopulatedRepositoryAsync()
         => Task.FromResult<IRepository<MysqlEntityMovie>>(new EntityTableRepository<MysqlEntityMovie>(Context));
