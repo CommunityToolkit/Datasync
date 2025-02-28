5b668450dc8880c678344a72ed4848ee32b28e1f
Adrian Hall <adhal@microsoft.com>
2025-02-28 09:39:10 -0800

    (#294) Client package uses NuGet

--- samples/todoapp/TodoApp.Uno/TodoApp.Uno/ViewModels/TodoListViewModel.cs
+++ samples/todoapp/TodoApp.Uno/TodoApp.Uno/ViewModels/TodoListViewModel.cs
@@ -2,15 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using CommunityToolkit.Datasync.Client;
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using Microsoft.EntityFrameworkCore;
 using TodoApp.Uno.Database;
+
+#nullable disable
 
 namespace TodoApp.Uno.ViewModels;
 
